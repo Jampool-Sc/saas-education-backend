0ee14bbc8ab2507f6294f9fa6f8e36bec768250d
djsanchez-dev <sanchezcoronadodeivy5@gmail.com>
2026-03-24 11:28:07 -0500

    first commit

---
+++ src/main/java/com/jampool/saas_academic/SaasAcademicApplication.java
@@ -0,0 +1,13 @@
+package com.jampool.saas_academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasAcademicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasAcademicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jampool/saas_academic/SaasAcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jampool.saas_academic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasAcademicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
